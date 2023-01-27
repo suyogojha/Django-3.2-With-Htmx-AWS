7cc8f0b12b65e5926b0665df51af7bda36a3751f
suyog ojha <ojhasuyog2020@gmail.com>
2023-01-27 08:45:58 +0545

    @Auto Form

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
+from django.forms.models import modelformset_factory # model form for querysets
 from django.shortcuts import redirect, render, get_object_or_404
 
-from .forms import RecipeForm
-from .models import Recipe
+from .forms import RecipeForm, RecipeIngredientForm
+from .models import Recipe, RecipeIngredient
 # CRUD -> Create Retrieve Update & Delete
 
 @login_required
@@ -41,11 +42,22 @@ def recipe_create_view(request):
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
+    # Formset = modelformset_factory(Model, form=ModelForm, extra=0)
+    RecipeIngredientFormset = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0)
+    qs = obj.recipeingredient_set.all() # []
+    formset = RecipeIngredientFormset(request.POST or None, queryset=qs)
     context = {
         "form": form,
+        "formset": formset,
         "object": obj
     }
-    if form.is_valid():
-        form.save()
+    if all([form.is_valid(), formset.is_valid()]):
+        parent = form.save(commit=False)
+        parent.save()
+        # formset.save()
+        for form in formset:
+            child = form.save(commit=False)
+            child.recipe = parent
+            child.save()
         context['message'] = 'Data saved.'
     return render(request, "recipes/create-update.html", context)  
